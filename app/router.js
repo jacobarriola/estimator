56b4fa3b6a104d669a2c562e97bce5e5028b88c0
Jacob Arriola <jacob@zeek.com>
2016-09-20 21:04:55 -0700

    Add routes

---
+++ app/router.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('estimates'); // /estimates
+  this.route('estimate', {path: 'estimate/:id'}, function() { // /estimate/estimate-name
+    this.route('tasks'); // /estimate/tasks
+  });
+  this.route('notfound', {path: '*path'}); // 404
+});
+
+export default Router;
