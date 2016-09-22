ae4025f7fcdf48d362d930701b860eea6b46aa67
Jacob Arriola <jacob@zeek.com>
2016-09-21 21:37:00 -0700

    Add favorite service

--- app/routes/estimates.js
+++ app/routes/estimates.js
@@ -1,12 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  favorites: Ember.inject.service(),
+
   model() {
     return [
       {title: "30-day-transformation"},
       {title: "fragd"},
       {title: "fashion-style-mag"}
     ];
+  },
+
+  actions: {
+    favoriteClicked(estimate) {
+      this.get('favorites').favoriteItem(estimate);
+    }
   }
 
 });
