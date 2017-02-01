b46c677560692bcfbc8bc7c0538abdadcae780fc
Jacob Arriola <jacob@zeek.com>
2017-01-31 20:56:08 -0800

    Set up task model with relationship to estimate

--- app/models/estimate.js
+++ app/models/estimate.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	title: DS.attr('string'),
 	content: DS.attr('string'),
-	status: DS.attr('string', { defaultValue: 'publish'})
+	status: DS.attr('string', { defaultValue: 'publish'}),
+	tasks: DS.hasMany('task')
 });
